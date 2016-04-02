fe367fc209ffb06319983bf72e5cb425c53f5752
Alan Jui <alanjui.1960@gmail.com>
2016-04-02 12:07:04 +0800

    加入可自 SideNav 選擇使用者功能；配合此選擇動作，能以點擊 ToolBar 的 Menu 按鈕，打開 SindeNav 的「選擇清單」；清單內顯示的使用者需能：（1）以英文字母排序；（2）使用者亦可透過「篩檢器」，透過文字輸入，快速找到自己要找的人；一旦點選「選擇清單」的某人之後，在 Main Content 窗格，當立即顯示「被選者」的個人資料

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -1,9 +1,26 @@
 /// <reference path="_all.ts" />
 
 namespace ContactManagerApp {
+  const URL_AVATAR_ICONS = './assets/svg/avatars.svg';
+  const URL_ICON_MENU = './assets/svg/menu.svg';
+  const URL_ICON_SHARE = './assets/svg/share.svg';
   
   angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons'])
     .service('userService', UserService)
-    .controller('mainController', MainController);
+    
+    .controller('mainController', MainController)
+    
+    .config(($mdThemingProvider: angular.material.IThemingProvider) => {
+      $mdThemingProvider.theme('default')         
+        .primaryPalette('blue')
+        .accentPalette('red');
+    })
+    
+    .config(($mdIconProvider: angular.material.IIconProvider) => {
+      $mdIconProvider
+        .defaultIconSet(URL_AVATAR_ICONS, 128)
+        .icon('menu', URL_ICON_MENU, 24)
+        .icon('share', URL_ICON_SHARE, 24);
+    });
 
 }
--- app/src/controllers/mainController.ts
+++ app/src/controllers/mainController.ts
@@ -4,6 +4,8 @@ namespace ContactManagerApp {
   
   export class MainController {
     users: User[] = [];
+    selectedUser: User = null;
+    searchText: string = '';
     message: string = 'Hello from our MainController';
     
     static $inject = ['userService', '$mdSidenav'];
@@ -16,13 +18,22 @@ namespace ContactManagerApp {
       
       userService.loadAllUsers()
         .then((users: User[]) => {
+          // console.log(self.users);  
           self.users = users;
-          console.log(self.users);  
+          self.selectedUser = self.users[0];
         });
     }
     
     toggleSideNav(): void {
       this.$mdSidenav('left').toggle();
+    }
+    
+    selectUser(user: User): void {
+      this.selectedUser = user;
+      
+      if (this.$mdSidenav('left').isOpen()) {
+        this.$mdSidenav('left').close();
+      }
     }
   }
   
